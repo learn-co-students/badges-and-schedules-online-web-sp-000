710de335aeaacdc5326495867f226be53b43b1be
Patrice Drayton <draytonpatrice@marilynssecret.org>
2018-12-13 21:39:00 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,38 @@
 # Write your code here.
+
+attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+
+
+def batch_badge_creator(array)
+  newarray = []
+  array.each do |name|
+    newarray.push("Hello, my name is #{name}.")
+  end
+ newarray
+end
+ 
+ 
+def assign_rooms(array)
+  nuarray = []
+  counter = 1
+  array.each do |name|
+    nuarray.push("Hello, #{name}! You'll be assigned to room #{counter}!")
+    counter += 1
+  end
+  return nuarray
+end
+
+def printer(array)
+  batch_badge_creator(array).each do |id|
+    puts id 
+  end 
+  
+  assign_rooms(array).each do |id|
+    puts id 
+  end
+end 
