71f4fd6a06404429fa6e79436351c6a1bdc627bd
Dr. XyClo <sdcarmody@gmail.com>
2018-12-31 15:47:47 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,28 +7,52 @@ end
 
 
 def batch_badge_creator(attendees)
- attendees_badge_array = []
+ @attendees_badge_array = []
  attendees.each do |attendees|
- attendees_badge_array << "Hello, my name is #{attendees}."
+ @attendees_badge_array << "Hello, my name is #{attendees}."
  
  end
  
- attendees_badge_array
+ @attendees_badge_array
  end 
  
+  def assign_rooms(attendees)
+   # rooms = [1,2,3,4,5,6,7]
+   room_assignments = []
+   # rooms.each do |attendees|
+   attendees.each_with_index do |attendees, index|
+     room_assignments << "Hello, #{attendees}! You'll be assigned to room #{index+1}!"
+   end
+  room_assignments
+ end
+ 
+  def printer(attendees) 
+     batch_badge_creator(attendees).each do |attendees|
+       puts attendees
+     end 
+   
+      
+    assign_rooms(attendees).each do |attendees|
+      puts attendees
+    end
+  end
+   
+   
+ 
+ 
+=begin  
  def assign_rooms(attendees)
    rooms = [1,2,3,4,5,6,7]
+   # room_assignments = []
    # rooms.each do |attendees|
-   attendees.each_with_index do |attendees|
-     room_assignments << "Hello, #{attendees}! You'll be assigned to room #{rooms}!"
+   attendees.collect.with_index do |attendees|
+     # room_assignments << 
+     "Hello, #{attendees}! You'll be assigned to room #{rooms}!"
    end
-   room_assignments
+  room_assignments
  end
+=end 
  
- def printer 
-   batch_badge_creator
-   assign_rooms
-   
- end 
+ 
    
    
