014c856eb1924979fb5c82c9d239d4d4e6d24b35
Corey H <c.l.halicke@gmail.com>
2019-02-12 00:48:07 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  badges = []
+  attendees.each do | name |
+    badges.push(badge_maker(name))
+  end
+  puts badges
+end
