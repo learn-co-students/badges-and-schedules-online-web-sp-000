357be048fa1a53c4f8bf7264308c418f2a4e894c
Corey H <c.l.halicke@gmail.com>
2019-02-12 00:55:18 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,8 +6,18 @@ end
 
 def batch_badge_creator(attendees)
   badges = []
-  attendees.each do |name|
+  attendees.each do | name |
     badges.push(badge_maker(name))
   end
   return badges
+end
+
+def assign_rooms(attendees)
+  room = 1 
+  assigned_room = []
+  attendees.each do | name |
+    assigned_room.push("Hello, #{name}! You'll be assigned to room #{room}")
+    room += 1 
+  end
+  return assigned_room
 end
