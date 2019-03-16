0a28fa72f8f25eda92299d3a9ecdce45dbe87354
Lauren McCoy <lmccoy8911@gmail.com>
2019-03-16 22:33:40 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,10 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end 
+
+def batch_badge_creator(attendees)
+  attendees = ["Edsger" , "Ada" , "Charles" , "Alan" , "Grace" , "Linus" , "Matz"]
+   attendees.each do |person|
+     batch_badge_creator(attendees)
+   end 
+end 
