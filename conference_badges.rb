2c7e1b4d62795082afd1cccac6d2d7a5529b9166
Lauren McCoy <lmccoy8911@gmail.com>
2019-03-17 22:24:02 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,9 +1,22 @@
+require 'pry'
 def badge_maker(name)
   return "Hello, my name is #{name}."
 end 
 
 def batch_badge_creator(attendees)
-   attendees.each {|person|
-      return "Hello, my name is #{person}."}.to_a 
+   attendees.map {|person|
+       "Hello, my name is #{person}."}
 end 
 
+def assign_rooms(attendees)
+  attendees.each_with_index.map {|attendees, index|
+   "Hello, #{attendees}! You'll be assigned to room #{index + 1}!"}
+end 
+
+def printer(attendees)
+   batch_badge_creator(attendees).each {|badge| 
+      puts badge }
+   assign_rooms(attendees).each {|room| 
+      puts room }
+   
+end 
