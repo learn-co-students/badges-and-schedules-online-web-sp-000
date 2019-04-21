3d33c06a2a3ba63a2ba0c48ef5c97e54f3f9a28c
Jim Hough <teachlife@outlook.com>
2019-04-21 23:28:52 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,22 @@
 # Write your code here.
+def badge_maker (name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator (array)
+  array.map do |name|
+     "Hello, my name is #{name}."
+  end
+end
+def assign_rooms (array)
+  room = 0
+  array.map do |name|
+    room +=1
+    "Hello, #{name}! You'll be assigned to room #{room}!"
+  end
+end
+
+def printer (array)
+  name_tags = batch_badge_creator(array)
+  puts name_tags
+end
