0a0f7efe001b1d18659ce37cd31c60c93531d980
Nicole Rothenay <nicoler@iccreativeagency.com>
2019-05-01 17:03:46 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,33 @@
 # Write your code here.
+
+# badge creator method creates name badge
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+# batch badge creator, takes array and returns new array
+def batch_badge_creator(attendees)
+# create empty array
+badge_messages = []
+# iterate over attendees & add to empty array
+attendees.each { |attendee| badge_messages << "Hello, my name is #{attendee}."}
+#return new array
+badge_messages
+end
+
+# assign rooms method assigns speakers to a room
+def assign_rooms(speakers)
+  #create empty array
+  room_assignments = []
+  #iterate over speakers & provide room assignment with #each_with_index
+  speakers.each_with_index { |speaker, index| room_assignments << "Hello, #{speaker}! You'll be assigned to room #{index + 1}!" }
+  #return new array
+  room_assignments
+end
+
+# printer method
+def printer(attendees)
+batch_badge_creator(attendees).each { |badges| puts "#{badges}"}
+assign_rooms(attendees).each { |rooms| puts "#{rooms}"}
+
+end
