1e240639a31e20542ff6533e9fc3a7b359d47a20
Connor Cowan <ccowan003@comcast.net>
2019-06-04 21:28:04 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,17 +3,17 @@
 attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 def badge_maker(name)
-  "Hello, my name is #{name}."
+  return "Hello, my name is #{name}."           #returns message from inputed array of attendees
 end
 
 def batch_badge_creator(attendees)
-  attendees.map do |attendee|
+  attendees.map do |attendee|                   #map iterator returns new array with results. in this case an array of badge messages
     "Hello, my name is #{attendee}."
   end
 end
 
 def assign_rooms(attendees)
-  attendees.each_with_index.map do |attendee, index|
+  attendees.each_with_index.map do |attendee, index|                    #each_with_index enumerator
     "Hello, #{attendee}! You'll be assigned to room #{index+1}!"
   end
 end
