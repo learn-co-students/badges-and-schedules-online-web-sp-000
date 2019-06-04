e992adda707cce3e7caee2d3463bbf6d0ea8c5da
Connor Cowan <ccowan003@comcast.net>
2019-06-04 20:59:12 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,17 @@
-# Write your code here.
+def badge_maker(name)
+
+  puts "Hello, my name is #{name}"
+
+
+  }
+puts "Hello my name is #{name}"
+end
+
+def batch_badge_creator(array_of_names)
+  return
+
+end
+
+def assign_rooms
+
+end
