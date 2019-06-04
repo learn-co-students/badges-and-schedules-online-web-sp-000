0c97636b71f9b1b7ddbb3edc43d8bf2b9a20113a
Connor Cowan <ccowan003@comcast.net>
2019-06-04 21:31:54 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,17 +13,17 @@ def batch_badge_creator(attendees)
 end
 
 def assign_rooms(attendees)
-  attendees.each_with_index.map do |attendee, index|                    #each_with_index enumerator
+  attendees.each_with_index.map do |attendee, index|                    #each_with_index enumerator: keeps track of the index number of the current iteration
     "Hello, #{attendee}! You'll be assigned to room #{index+1}!"
   end
 end
 
 def printer(attendees)
-  batch_badge_creator(attendees).each do |badge|
+  batch_badge_creator(attendees).each do |badge|            #printer method, outputs results of batch_badge_creator call
     puts badge
   end
 
-  assign_rooms(attendees).each do |assignment|
+  assign_rooms(attendees).each do |assignment|              #then assign_rooms call
     puts assignment
   end
 end
