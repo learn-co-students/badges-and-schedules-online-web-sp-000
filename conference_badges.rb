dc3b421baff0c794080ee61728bbe336c9c22f76
Chris Cheong <chrisxcheong@gmail.com>
2019-07-10 04:23:54 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,10 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  batch = []
+  array.each do |name| {batch << "Hello, my name is #{name}."}
+
+end
