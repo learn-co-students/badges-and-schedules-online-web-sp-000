5ab26371928d6fec9f7f5b28432ac150d912d0d9
github username <github email address>
2019-09-24 11:10:20 -0400

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,38 +1,30 @@
-require'pry'
+
 # Write your code here.
+require'pry'
+attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
-the_people = [Edsger, Ada, Charles, Alan, Grace, Linus, Matz]
-
-def badge_maker(NameError)
+def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-
-def batch_badge_creator(the_people)
-  the_people.map do |the_people|
-    "Hello, my name is #{the_people}"
+def batch_badge_creator(attendees)
+  attendees.map do |attendee|
+    "Hello, my name is #{attendee}."
   end
 end
 
-# def each_with_index(the_people)
-#   the_people.map do |the_people|
-#         "Hello, my name is #{the_people}"
-#   end
-# end
-
-def assign_rooms(the_people)
-  the_people.each_with_index.map do |the_people, index|
-        "Hello, #{the_people}! Your room is #{index + 1}!"
+def assign_rooms(attendees)
+  attendees.each_with_index.map do |attendee, index|
+    "Hello, #{attendee}! You'll be assigned to room #{index+1}!"
   end
 end
 
-def printer(the_people)
-  batch_badge_creator(the_people).each do |badge|
+def printer(attendees)
+  batch_badge_creator(attendees).each do |badge|
     puts badge
   end
 
-
-  assign_rooms(the_people).each do |assignment|
+  assign_rooms(attendees).each do |assignment|
     puts assignment
   end
 end
