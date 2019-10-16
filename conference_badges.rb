c93adce3420e8df52eb37d9eb49a83ca09f5dc38
Aoune Syed <aoune007@gmail.com>
2019-10-16 05:48:50 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,3 +15,13 @@ def assign_rooms(speakers)
     "Hello, #{speaker}! You'll be assigned to room #{index + 1}!"
   end
 end
+
+def printer(speakers)
+  batch_badge_creator(speakers).each do |badge|
+  puts badge
+  end
+
+  assign_rooms(speakers).each do |assignment|
+    puts assignment
+  end
+end
