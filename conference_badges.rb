673b027d833901a6f598231631a3bad7e1f01057
Daniel Cowpertwait <danielcowpertwait@gmail.com>
2020-01-24 04:50:45 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,35 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  badges = []
+  attendees.each do |nameofperson|
+    badges << (badge_maker(nameofperson))
+  end
+  badges
+end 
+
+def assign_rooms(attendees)
+  rooms = []
+  attendees.each_with_index do |attendee, index|
+    rooms << ("Hello, #{attendee}! You'll be assigned to room #{index + 1}!")
+  end
+  rooms
+end
+
+def printer(attendees)
+  badges = batch_badge_creator(attendees)
+  room_assignment = assign_rooms(attendees)
+  badges.each do |yourbadge|
+    puts yourbadge
+  end
+  room_assignment.each do |yourbadge|
+    puts yourbadge
+    
+  end
+  
+end
+
+
+
