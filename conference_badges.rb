707567f3768801c0c012158bef7f501098e46e3d
Jennifer Pfeffer <jennifer.pfeffer127@gmail.com>
2020-01-26 16:49:09 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,16 @@
 # Write your code here.
+def badge_maker(name) 
+  return "Hello, my name is #{name}." 
+end
+def batch_badge_creator(attendees) 
+  badges = []
+  attendees.each {|name| badges.push("Hello, my name is #{name}.")}
+    return badges
+end
+def assign_rooms(attendees,room) 
+  list = [] 
+  attendees.each { |name| list.push("Hello, #{name}! You will be assigned to room #{room}!")
+  return list 
+end 
+def printer(batch_badge_creator,assign_rooms) 
+end
