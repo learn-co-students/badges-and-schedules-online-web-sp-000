719169def3a13d5b5c0e892d1c80b34bdc68dfe8
Jennifer Pfeffer <jennifer.pfeffer127@gmail.com>
2020-01-26 16:53:56 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,10 +7,17 @@ def batch_badge_creator(attendees)
   attendees.each {|name| badges.push("Hello, my name is #{name}.")}
     return badges
 end
-def assign_rooms(attendees,room) 
+def assign_rooms(attendees) 
   list = [] 
-  attendees.each { |name| list.push("Hello, #{name}! You will be assigned to room #{room}!")
+  attendees.each_with_index.map { |name,index| list.push("Hello, #{name}! You'll be assigned to room #{index + 1}!")}
   return list 
 end 
-def printer(batch_badge_creator,assign_rooms) 
+def printer(attendees) 
+  batch_badge_creator(attendees).each do |badge|
+    puts badge
+  end
+
+  assign_rooms(attendees).each do |assignment|
+    puts assignment
+  end
 end
