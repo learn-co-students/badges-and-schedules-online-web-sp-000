5eef4e6a341458b7c671246d6ce7bc521e23e02d
C. Leow <chienleow1@gmail.com>
2020-03-01 16:16:55 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,20 +3,26 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(attendees)
-  list_of_badge_messages = []
+  badge_messages = []
   attendees.each do |name|
-  list_of_badge_messages << badge_maker(name)
+  badge_messages << badge_maker(name)
   end
-  list_of_badge_messages
+  badge_messages
 end
 
 def assign_rooms(attendees)
   room_number = 0
-  while room_number < 7
-  room_number += 1
-  return "Hello, #{attendees}! You'll be assigned to room #{room_number}!"
-end
+  while room_number < attendees.size
+  	room_number += 1
+  	welcome = []
+  	attendees.each do |name|room_number|
+  	puts "Hello, #{name}! You'll be assigned to room #{room_number}!"
+  end
+  	welcome
+  end
 end
 
 def printer
+  puts batch_badge_creator(attendees)
+  puts assign_rooms(attendees)
 end
