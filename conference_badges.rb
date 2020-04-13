cb6e5df4da9dcd4bcb0486d138b2da35fb11e9f1
proactive-analyst-6157 <u.batsaikhan@gmail.com>
2020-04-13 04:08:47 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,30 @@
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(name_list)
+  new_name_list = []
+  name_list.each do |name|
+    new_name_list << "Hello, my name is #{name}."
+  end
+  return new_name_list
+end
+
+def assign_rooms(name_list)
+  new_name_list = []
+  name_list.each do |name|
+    new_name_list << "Hello, #{name}! You'll be assigned to room #{name_list.index(name)+1}!"
+  end
+  return new_name_list
+end
+
+def printer(attendees)
+  batch_badge_creator(attendees).each do |array|
+    puts array
+  end
+  assign_rooms(attendees).each do |array|
+    puts array
+  end
+end
+
