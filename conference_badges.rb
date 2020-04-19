51f095ba34efc3b923f407bb823329739f512c1b
Juan-Sergio Khoshdel <jsergiofm85@gmail.com>
2020-04-19 04:58:46 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,25 +1,24 @@
 require 'pry'
+
 def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(arr)
-  new_arr = []
-  arr.each {|name| new_arr << "Hello, my name is #{name}."}
-  new_arr
+def batch_badge_creator(list)
+  list.map {|name| "Hello, my name is #{name}."}
 end
 
-def assign_rooms(arr)
-  arr.each_with_index.map do
-    |name, room| "Hello, #{name}! You'll be assigned to room #{room+1}!"
-  end
+def assign_rooms(guest)
+  guest.map.with_index {|name, room|
+    "Hello, #{name}! You'll be assigned to room #{room+1}!"
+  }
 end
 
 def printer(guest)
-  batch_badge_creator(guest).each do |badge|
+  batch_badge_creator(guest).map do |badge|
     puts badge
   end
-  assign_rooms(guest).each do |room|
+  assign_rooms(guest).map do |room|
     puts room
-  end
+  end  
 end
