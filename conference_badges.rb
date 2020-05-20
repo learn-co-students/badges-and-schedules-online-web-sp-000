bc20909c41fc22a522f9a42bff5f6637f6880a2c
Kristina Frederick <kristinafrederick@icloud.com>
2020-05-20 22:43:29 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+def badge_maker(name = "Arel")
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  attendees.map do |attendee|
+    "Hello, my name is #{attendee}."
+  end
+end
+
+def assign_rooms(attendees)
+  results = []
+  attendees.each_with_index do |attendee, index|
+    results.push("Hello, #{attendee}! You'll be assigned to room #{index.to_i+1}!")
+  end
+  results
+end
+
+def printer(attendees)
+  batch_badge_creator(attendees).each do |attendee|
+    puts attendee
+  end
+  assign_rooms(attendees).each do |attendee|
+    puts attendee
+  end
+end
