3316e540cb0763cd4ef8f304289491cc83b67603
reevesalic <reevesalic@gmail.com>
2020-06-13 23:03:22 -0400

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,9 +11,19 @@ def batch_badge_creator(attendees)
   return messages
 end
 
-def assign_rooms(speaker)
- greeting = []   
-speaker.each_with_index |speaker, index|
-    greeting << "Hello #{speaker}! You'll be assigned to #{index}"
-    return greeting
+def assign_rooms(speakers)
+  greet = []
+  speakers.each_with_index{ |speakers, index| greet << "Hello, #{speakers}! You'll be assigned to room #{index+1}!"}
+  return greet
+  end
+
+  def printer(attendees)
+    batch_badge_creator(attendees).each do |badge|
+    puts badge
+  end
+
+  assign_rooms(attendees).each do |badge|
+    puts badge
+  end
 end
+
