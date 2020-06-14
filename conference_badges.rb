f01287853030138e02e9048713f4eaf0d5ff1aa9
reevesalic <reevesalic@gmail.com>
2020-06-13 20:51:35 -0400

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+# Write your code here
+def badge_maker(name)
+ return "Hello, my name is #{name}."
+end
+  
+def batch_badge_creator(attendees)
+   messages = []
+   attendees.each do |name|
+   messages << badge_maker(name)
+  end
+  return messages
+end
