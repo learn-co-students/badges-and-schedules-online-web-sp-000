232b39358bdb8a873d7d685457c475bd2d3ca4c2
Jean-Victor Brun <jeanvbrun@icloud.com>
2020-07-13 19:59:09 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,10 @@
-# Write your code here.
+require_relative './spec/conference_badges_spec.rb'
+
+names = ["Edsger","Ada","Charles","Alan","Grace","linus","Matz"]
+def batch_badge_creator(names)
+  names.each do |name| puts "Hello, my name is #{name}"
+end
+
+def assign_rooms(names)
+  names.each do |name| puts "Hello, #{name}! You'll be assigend to room #{names.index+1}"
+end
