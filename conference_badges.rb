4c74bad59006d283eaab37dfe8ff04ac1d24b679
shacoca <shacoca@yahoo.com>
2020-08-11 20:42:08 -0700

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,9 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(name_array)
+  message_array = []
+  name_array.collection{ |name| message_array << badge_maker(name)}
+  message_array
+end
