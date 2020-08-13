8f06abb932caafebaadacead9dff2f425e29e68a
shacoca <shacoca@yahoo.com>
2020-08-12 23:54:00 -0700

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,6 +4,23 @@ end
 
 def batch_badge_creator(name_array)
   message_array = []
-  name_array.collection{ |name| message_array << badge_maker(name)}
+  name_array.collect{ |name| message_array << badge_maker(name)}
   message_array
+end
+
+def assign_rooms(names)
+    # takes the list of speakers and assigns each speaker to a room. Make sure that each room only has one speaker.
+    assignments = []
+    names.each_with_index {|name, index| assignments << "Hello, #{name}! You'll be assigned to room #{index + 1}!"}
+    assignments
+end
+
+def printer(attendees)
+    attendees.each_with_index do |attendee, index|
+        puts batch_badge_creator(attendees)[index]
+    end
+
+    attendees.each_with_index do |attendee, index|
+        puts assign_rooms(attendees)[index]
+    end
 end
