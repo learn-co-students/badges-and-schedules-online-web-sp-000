e0595b22e2fb29f2f54098e713984b39d3b5895a
modern-class-9458 <alexander.clamor@gmail.com>
2020-08-20 19:55:24 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,22 @@
-# Write your code here.
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  array.map{|name| "Hello, my name is #{name}."}
+end
+
+def assign_rooms(array)
+  array.each_with_index.map {|name, i|
+    "Hello, #{name}! You'll be assigned to room #{i+1}!"
+  }
+end
+
+def printer(array)
+  batch_badge_creator(array).each{|s|puts s}
+  assign_rooms(array).each{|s| puts s}
+end
+
+array = ["A","B"]
+printer(array)
