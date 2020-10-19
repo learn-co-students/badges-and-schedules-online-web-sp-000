f207559ad175f4acfa0e09cf2ce978969f3e41f3
Damian Malysza <damian.malysza@gmail.com>
2020-10-19 01:45:15 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,27 @@
 # Write your code here.
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+
+def batch_badge_creator(array)
+  array.collect do |i|
+    badge_maker(i)
+  end
+end
+
+def assign_rooms(array)
+  array.collect do |name|
+    "Hello, #{name}! You'll be assigned to room #{array.index(name)+1}!"
+  end
+end
+
+def printer(array)
+  batch_badge_creator(array).each do |i|
+    puts i
+  end
+  assign_rooms(array).each do |i|
+    puts i
+  end
+end
