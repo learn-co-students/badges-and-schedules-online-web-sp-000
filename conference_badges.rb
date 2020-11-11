cb431c16c0734097781c40f72093382e30571cb0
Benjamin Cheng <bencheng200@gmail.com>
2020-11-11 16:18:42 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,35 @@
 # Write your code here.
+def badge_maker (name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  final = []
+  array.each do |x|
+    final.push(badge_maker(x))
+  end
+  final
+end
+
+def assign_rooms(array)
+  final = []
+  room = 1
+    array.each do |x|
+    final.push("Hello, #{x}! You'll be assigned to room #{room}!")
+    room += 1
+  end
+final
+end
+
+def printer(array)
+  wow = batch_badge_creator(array)
+  wow1 = assign_rooms(array)
+  i=0
+  
+  while i<wow.length
+    puts wow[i]
+    puts wow1[i]
+    i += 1
+  end
+end
+  
