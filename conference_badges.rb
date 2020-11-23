f25e7db2861d4ca1d7240c70301fe36c0d29a39b
HELEN HARMAN <hlharman@bellsouth.net>
2020-11-23 19:18:02 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,19 @@
-# Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end 
+
+def batch_badge_creator(attendees)  
+badges = " "
+attendees.each do |attendee|
+badges >> "Hello, my name is #{attendee}"
+end 
+badges
+end 
+
+def printer
+  badges_and_room_assignments.each_line do |line|
+    puts line.chomp
+      end
+      printer(attendees)
+    end  
+    
