cbfea3c439f6487a1ab7e97cd08e2c482d3197b2
danpaslawsky <danpaslawsky@gmail.com>
2021-03-12 12:45:03 -0800

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+def badge_maker(name)
+    puts "Hello, my name is #{name}."
+end
+
+#def assign_rooms(names)
+    #array.each do |name|
+    #"Hello, #{names}! You'll be assigned to room  !"
+##end
+
+#def printer
+
+#end
