b66b0d8a81d997f6a1d8647c5ec483645ed61aa5
Christopher Black <christopher.black.2209@googlemail.com>
2021-03-31 23:13:34 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,9 +3,7 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(speaker_names)
-  badge_messages = []
-  speaker_names.each {|name| badge_messages << "Hello, my name is #{name}."}
-  return badge_messages
+  speaker_names.collect {|name| "Hello, my name is #{name}."}
 end
 
 def assign_rooms(speaker_names)
@@ -17,7 +15,7 @@ def assign_rooms(speaker_names)
   return assigned_rooms
 end
 
-def printer
+def printer(attendees)
   batch_badge_creator.each {|badge| puts "#{badge}"}
   assign_rooms.each {|assigned_room| puts "#{assigned_room}"}
 end
