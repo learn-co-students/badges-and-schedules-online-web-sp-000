43bda961fa3bbcce84cd26d3921ee2f0c84ebe25
Christopher Black <christopher.black.2209@googlemail.com>
2021-03-31 16:18:21 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,3 +1,9 @@
 def badge_maker(name)
   return "Hello, my name is #{name}."
 end
+
+def batch_badge_creator(badge_names)
+  badge_messages = []
+  badge_names.each {|name| badge_messages << "Hello, my name is #{name}."}
+  return badge_messages
+end
