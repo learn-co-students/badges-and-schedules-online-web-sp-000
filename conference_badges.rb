6fc8a333714b8a5f7721222ad82cdc88a6cbbee1
Dorthy P Thielsen <dotinmotionproductions@gmail.com>
2021-05-11 18:27:29 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,20 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  badge=[]
+  attendees.each do |message|
+    badge << badge_maker(message)
+  end
+  return badge
+end
+
+def assign_rooms(attendees)
+ room_assignments=[1,2,3,4,5,6,7]
+ %w(attendees).each_with_index { |
+end
+
+def printer(batch_badge_creator, assign_rooms)
+
+end
